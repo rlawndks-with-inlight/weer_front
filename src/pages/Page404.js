70d282c473d5db0c75e51b2aa1ea5dc283aef6c0
rlawndks4 <rlawndks0423@naver.com>
2023-03-22 11:55:44 +0900

    asdf

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { Wrappers } from "../components/elements/UserContentTemplete";
 import styled from "styled-components";
 import { MdOutlineCancel } from 'react-icons/md'
 import theme from "../styles/theme";
+import { frontUrl } from "../data/Data";
 const RowContent = styled.div`
 display:flex;
 width:100%;
@@ -13,12 +14,13 @@ margin:24px 0;
 `
 const Page404 = () => {
     const navigate = useNavigate();
-
+    const {pathname} = useLocation();
     const [posts, setPosts] = useState({});
     return (
         <>
             <Wrappers>
                     <div style={{ margin: 'auto auto 8px auto' }}>없는 페이지 입니다.</div>
+                    <div style={{ margin: '8px auto 8px auto' }}>{frontUrl}{pathname}</div>
                     <MdOutlineCancel style={{ margin: '8px auto auto auto', fontSize: '54px' }} />
             </Wrappers>
         </>
